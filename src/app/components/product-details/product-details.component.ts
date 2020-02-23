a7bb62a24b622f094e36e3775367e58a71cc31d2
Varun Sukheja <varun.sukheja@myglamm.com>
2020-02-24 00:41:40 +0530

    fetched product details

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+// Services
+import { ProductService } from 'src/app/services/product.service';
+import { ProductI } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
+  productId: number;
+  productDetails: ProductI;
 
-  constructor() { }
+  constructor(
+    private _route: ActivatedRoute,
+    private _productService: ProductService
+  ) { }
 
+  /**
+   * @description we extract route param(product id) from URL and 
+   * then we get the product details for that product id.
+   *
+   * @memberof ProductDetailsComponent
+   */
   ngOnInit() {
+    this.productId = this._route.snapshot.params['id'];
+    this.fetchProductDetails();
   }
 
+  /**
+   * @description it is used to fetch the product details
+   *
+   * @memberof ProductDetailsComponent
+   */
+  fetchProductDetails() {
+    this._productService.fetchProductById(this.productId)
+      .subscribe(resp => this.productDetails = resp);
+  }
+
+  
 }

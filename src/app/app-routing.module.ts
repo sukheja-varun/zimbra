ea17d29aaa10a8bc6dd58717403df82a51c00f68
Varun Sukheja <varun.sukheja@myglamm.com>
2020-02-23 22:05:47 +0530

    created components: - not-found - product listing - product details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProductListingComponent } from './components/product-listing/product-listing.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListingComponent },
+  { path: 'product/:id', component: ProductDetailsComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProductListingComponent } from './components/product-listing/product-listing.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    ProductListingComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
